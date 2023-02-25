49101bbaed1a5221595bce84aafc43096137cdd8
Akhil Sharma <desire.akhil@gmail.com>
2023-02-25 18:04:41 +0530

    add integration for github

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -14,13 +14,16 @@ export default function Socials() {
                     <i class="social-logo fa fa-linkedin-square" aria-hidden="true"></i>
                 </a>
                 <a className='social' href="https://topmate.io/akhilsharma" target="_blank" rel="noreferrer">
-                    <i class="social-logo fa-solid fa-screen-users" aria-hidden="true"></i>
+                    <i class="social-logo fa-square-odnoklassniki" aria-hidden="true"></i>
                 </a>
                 <a className='social' href="https://twitter.com/akhilsharma_" target="_blank" rel="noreferrer">
                     <i class="social-logo fa fa-twitter-square" aria-hidden="true"></i>
                 </a>
+                <a className='social' href="https://github.com/akhilvatts" target="_blank" rel="noreferrer">
+                    <i class="social-logo fa fa-square-github" aria-hidden="true"></i>
+                </a>
                 <a className='social' href="https://instagram.com/akhil_vatts" target="_blank" rel="noreferrer">
-                    <i class="social-logo fa fa-instagram" aria-hidden="true"></i>
+                    <i class="social-logo fa fa-square-instagram" aria-hidden="true"></i>
                 </a>
             </div>
         </div>
